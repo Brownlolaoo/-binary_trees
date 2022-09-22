21cb1b9a11926e943686d5f16a7051548e52cba9
mac <user@macs-Air.realtek>
2022-09-22 16:37:54 +0800

    BINARY TREES

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * binary_tree_node - creates a binary tree node
@@ -6,18 +7,16 @@
  * @value: value to put in the new node
  *
  * Return: Pointer to the newly created node
- *         NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+	new_node->parent = parent;
+        new_node->n = value;
+        new_node-> left = new_node->right = NULL;
 	return (new);
 }
